0927202a57951f2f7b64f9582ed0726a7ec20058
Thomas Barthelemy <thomasb@gobeta.com.cn>
2015-11-23 08:22:20 +0700

    T334: integrate assets to bhbx website
    Reviewers: soum1, thomasb
    
    Reviewed By: thomasb
    
    Projects: #bhbx
    
    Maniphest Tasks: T334
    
    Differential Revision: http://phabricator.we-bridge.net/D323

--- js/i18n.js
+++ js/i18n.js
@@ -1,7 +1,7 @@
 // Create language switcher instance and set default language to en
-window.lang = new Lang('en');
+window.lang = new Lang('zh');
 
 // Define the chinese language pack as a dynamic pack to be loaded on demand
 // if the user asks to change to that language. We pass the two-letter language
 // code and the path to the language pack js file
-window.lang.dynamic('zh', 'bower_components/jquery-lang-js/js/langpack/zh.json');
+window.lang.dynamic('en', 'languages/en.json');
